c90f2195908f590f023518257d25f7e108a3004d
Stan Lo <stan.lo@shopify.com>
2022-12-14 12:49:41 +0000

    Use IntegrationTestCase to provide end-to-end testing capability.

--- test/irb/helper.rb
+++ test/irb/helper.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
+require "pty" unless RUBY_ENGINE == 'truffleruby'
+
 require "test/unit"
 require "pathname"
+require "tempfile"
+require "tmpdir"
+require "envutil"
 
 begin
   require_relative "../lib/helper"
@@ -71,6 +78,119 @@ module TestIRB
       rescue LoadError # ruby/ruby defines EnvUtil differently
       end
       EnvUtil.suppress_warning { ::Kernel.send(:alias_method, :require, :old_require) }
+    end
+  end
+
+  class IntegrationTestCase < TestCase
+    IRB_ENVS = { "NO_COLOR" => "true" }.freeze
+    TIMEOUT_SEC = 3
+    LIB = File.expand_path("../../lib", __dir__)
+
+    def setup
+      if ruby_core?
+        omit "This test works only under ruby/irb"
+      end
+
+      if RUBY_ENGINE == 'truffleruby'
+        omit "This test requires the `pty` library, which doesn't work with truffleruby"
+      end
+
+      @irb_envs = IRB_ENVS
+    end
+
+    private
+
+    def run_ruby_file(&block)
+      cmd = [EnvUtil.rubybin, "-I", LIB, @ruby_file.to_path]
+      tmp_dir = Dir.mktmpdir
+      rc_file = File.open(File.join(tmp_dir, ".irbrc"), "w+")
+      rc_file.write("IRB.conf[:USE_SINGLELINE] = true")
+      rc_file.close
+
+      @commands = []
+      lines = []
+
+      yield
+
+      PTY.spawn(@irb_envs.merge("IRBRC" => rc_file.to_path), *cmd) do |read, write, pid|
+        Timeout.timeout(TIMEOUT_SEC) do
+          while line = safe_gets(read)
+            lines << line
+
+            # means the breakpoint is triggered
+            if line.match?(/binding\.irb/)
+              while command = @commands.shift
+                write.puts(command)
+              end
+            end
+          end
+        end
+      ensure
+        read.close
+        write.close
+        kill_safely(pid)
+      end
+
+      lines.join
+    rescue Timeout::Error
+      message = <<~MSG
+      Test timedout.
+
+      #{'=' * 30} OUTPUT #{'=' * 30}
+        #{lines.map { |l| "  #{l}" }.join}
+      #{'=' * 27} END OF OUTPUT #{'=' * 27}
+      MSG
+      assert_block(message) { false }
+    ensure
+      File.unlink(@ruby_file) if @ruby_file
+      FileUtils.remove_entry tmp_dir
+    end
+
+    # read.gets could raise exceptions on some platforms
+    # https://github.com/ruby/ruby/blob/master/ext/pty/pty.c#L729-L736
+    def safe_gets(read)
+      read.gets
+    rescue Errno::EIO
+      nil
+    end
+
+    def kill_safely pid
+      return if wait_pid pid, TIMEOUT_SEC
+
+      Process.kill :TERM, pid
+      return if wait_pid pid, 0.2
+
+      Process.kill :KILL, pid
+      Process.waitpid(pid)
+    rescue Errno::EPERM, Errno::ESRCH
+    end
+
+    def wait_pid pid, sec
+      total_sec = 0.0
+      wait_sec = 0.001 # 1ms
+
+      while total_sec < sec
+        if Process.waitpid(pid, Process::WNOHANG) == pid
+          return true
+        end
+        sleep wait_sec
+        total_sec += wait_sec
+        wait_sec *= 2
+      end
+
+      false
+    rescue Errno::ECHILD
+      true
+    end
+
+    def type(command)
+      @commands << command
+    end
+
+    def write_ruby(program)
+      @ruby_file = Tempfile.create(%w{irb- .rb})
+      @ruby_file.write(program)
+      @ruby_file.close
     end
   end
 end
